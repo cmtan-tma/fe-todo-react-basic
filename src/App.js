60e8c4332cf8b028deacd55800cedf1a3df7bd78
chuminhtan <chuminhtan.work@gmail.com>
2022-06-30 20:18:30 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import {FaTasks, FaCheckDouble, FaBusinessTime} from 'react-icons/fa'
 import './App.css';
 
-function App() {
+const App = () => {
+  const list = [1,2,3,4,5,6,7,8,9]
+  const list2 = [5];
+  const createTasks = (list) => {
+
+    if (list.length === 1) {
+      return (<div className='task'>Task number 1</div>)
+    }
+
+    const taskLists = [];
+    const length = list.length
+    for (let i = 0; i < length; i++) {
+      let classes = 'task border-bottom'
+      if (i === length - 1) {
+        classes = 'task';
+      }
+      const taskElement = (<div className={classes} key={i.toString()}>Task number {i}</div>)
+      taskLists.push(taskElement)
+    }
+
+    return taskLists
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="body">
+          <div className="header">
+            <h1>Todos</h1>
+          </div>
+          <div className="task-list">
+              {
+                createTasks(list)
+              }
+          </div>
+          <div className="input-bar">
+              <input 
+                type="text"
+                placeholder='Enter your task'
+
+                />
+          </div>
+          <div className="button-control">
+              <div>                
+                <p className='icon'><FaBusinessTime/></p>
+                <p>Tasks</p>
+              </div>
+              <div>
+                <p className='icon'><FaCheckDouble /></p>
+                <p>Completed</p>
+              </div>
+              <div>
+                <p className='icon'><FaTasks/></p>
+                <p>All</p>
+              </div>
+          </div>
+        </div>
     </div>
   );
 }
